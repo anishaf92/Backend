d87d41077efa9e3449c5d0175be40273ccdf98ef
unknown <anishaf92@gmail.com>
2024-12-12 13:03:22 +0530

    atlas used

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const userRoutes = require('./routes/userRoutes')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
 const cors = require('cors');
+const app = express();
+
 app.use(cors());
-const port = 3000
-mongoose.connect('mongodb://localhost:27017/userDetails')
+const port = 3000;
+username = 'root'
+password = 'Shahid@2410'
+// Correct connection string with no port number
+mongoose.connect(
+  `mongodb+srv://anishaf92:shahid2410@cluster0.togbb5m.mongodb.net/?retryWrites=true&w=majority`)
+  
 .then(() => console.log('Connected to DB'))
-.catch((err) => console.log(err))
-app.use(express.json())
-app.use('/user',userRoutes)
+.catch((err) => console.log('Error connecting to DB:', err));
 
-app.listen(port,(err) => {
-    if(!err){
-        console.log('Server is connected to port no ',port)
-    }
-    else{
-        console.log(err)
-    }
-})
+app.use(express.json());
+app.use('/user', userRoutes);
 
-
+app.listen(port, (err) => {
+  if (!err) {
+    console.log('Server is connected to port no ', port);
+  } else {
+    console.log('Error starting server:', err);
+  }
+});
